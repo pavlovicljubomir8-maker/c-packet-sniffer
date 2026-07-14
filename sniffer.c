a6ea80a2cd280b2d5f78224d9c3ae22e8ec1c0a9
pavlovicljubomir8-maker <pavlovicljubomir8@gmail.com>
2026-07-14 17:43:47 +0200

    SIGINT signal handler

--- sniffer.c
+++ sniffer.c
@@ -6,10 +6,19 @@
 #include <arpa/inet.h>
 #include <netinet/if_ether.h>
 #include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <signal.h>
+#include <unistd.h>
+volatile sig_atomic_t running = 1;
 
+void handle_sigint(int sig) {
+    running = 0;
+}
 
 int main (void) {
 
+    signal(SIGINT, handle_sigint);
     int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sock < 0) {
         perror("socket");
@@ -18,7 +27,7 @@ int main (void) {
 
     unsigned char buffer[65536];
 
-    while (1) {
+    while (running) {
         int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, NULL, NULL);
         if (bytes < 0) {
             perror("recvfrom");
@@ -32,10 +41,30 @@ int main (void) {
         src.s_addr = ip->saddr;
         dst.s_addr = ip->daddr;
 
-        printf("Protocol %d | from %s", ip->protocol, inet_ntoa(src));
-        printf(" to %s\n", inet_ntoa(dst));
 
+        int ip_header_len = ip-> ihl *4;
+
+        if (bytes < 14 +ip_header_len + (int)sizeof(struct tcphdr)) continue;
+
+        if (ip->protocol == 6){
+            if(bytes < 14 + ip_header_len + (int)sizeof(struct tcphdr)) continue;
+            struct tcphdr *tcp = (struct tcphdr *)(buffer + 14 + ip_header_len);
+             printf("TCP %s:%d -> ", inet_ntoa(src), ntohs(tcp->source));
+             printf("%s:%d\n", inet_ntoa(dst), ntohs(tcp->dest));
+        }
+        else if (ip->protocol == 17){
+            if (bytes < 14 + ip_header_len + (int)sizeof(struct udphdr)) continue;
+            struct udphdr *udp = (struct udphdr *)(buffer + 14 + ip_header_len);
+            printf("UDP %s:%d -> ", inet_ntoa(src), ntohs(udp->source));
+            printf("%s:%d\n", inet_ntoa(dst), ntohs(udp->dest));
+        }
+        else if( ip->protocol == 1) {
+        printf("ICMP %s -> ", inet_ntoa(src));
+        printf("%s\n", inet_ntoa(dst));
+        }
 
     }
+    close(sock);
+    printf("\nShutting down. Bye untill next time.\n");
     return 0;
 } 
