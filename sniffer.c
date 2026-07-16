4cb8dedbc95d8898bd584a7719d56960059da1b9
pavlovicljubomir8-maker <pavlovicljubomir8@gmail.com>
2026-07-16 12:55:09 +0200

    packet statistics

--- sniffer.c
+++ sniffer.c
@@ -27,12 +27,19 @@ int main (void) {
 
     unsigned char buffer[65536];
 
+    int tcp_count = 0;
+    int udp_count = 0;
+    int icmp_count = 0;
+    int total_count = 0;
+
     while (running) {
         int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, NULL, NULL);
         if (bytes < 0) {
             perror("recvfrom");
             break;
         }
+
+        total_count ++; 
         
         if (bytes < 14 + (int)sizeof(struct iphdr)) continue;
         
@@ -49,20 +56,24 @@ int main (void) {
             struct tcphdr *tcp = (struct tcphdr *)(buffer + 14 + ip_header_len);
              printf("TCP %s:%d -> ", inet_ntoa(src), ntohs(tcp->source));
              printf("%s:%d\n", inet_ntoa(dst), ntohs(tcp->dest));
+             tcp_count ++;
         }
         else if (ip->protocol == 17){
             if (bytes < 14 + ip_header_len + (int)sizeof(struct udphdr)) continue;
             struct udphdr *udp = (struct udphdr *)(buffer + 14 + ip_header_len);
             printf("UDP %s:%d -> ", inet_ntoa(src), ntohs(udp->source));
             printf("%s:%d\n", inet_ntoa(dst), ntohs(udp->dest));
+            udp_count ++;
         }
         else if( ip->protocol == 1) {
         printf("ICMP %s -> ", inet_ntoa(src));
         printf("%s\n", inet_ntoa(dst));
+        icmp_count ++;
         }
-
     }
     close(sock);
+    printf("TCP: %d UDP: %d ICMP: %d\n", tcp_count, udp_count, icmp_count);
+    printf("Total packets: %d\n", total_count);
     printf("\nShutting down. Bye untill next time.\n");
     return 0;
 } 
