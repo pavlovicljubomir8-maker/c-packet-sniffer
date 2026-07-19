a73495892bfcb9d0f0ea6464b6c03ad5f35e577a
pavlovicljubomir8-maker <pavlovicljubomir8@gmail.com>
2026-07-19 19:23:03 +0200

    TLS SNI

--- sniffer.c
+++ sniffer.c
@@ -50,6 +50,48 @@ void parse_dns_name(unsigned char *buffer, int bytes, int start, int dns_start) 
     printf("\n");
 }
 
+void parse_tls_sni(unsigned char *buffer, int bytes, int tls_offset, char *timestr){
+    int pos = tls_offset;
+
+    pos += 5 + 4;
+    pos += 2;
+    pos += 32;
+    if (pos + 1 >bytes) return;
+    int session_id_len = buffer[pos];
+    pos += 1 + session_id_len;
+
+    if (pos + 2 > bytes) return;
+    int cipher_len = (buffer[pos] << 8) | buffer[pos + 1];
+    pos += 2 + cipher_len;
+
+    if (pos + 1 > bytes) return;
+    int com_len = buffer[pos];
+    pos += 1 + com_len;
+
+    if(pos + 2 > bytes) return;
+    pos += 2;
+
+    while (pos + 4 <= bytes) {
+        int ext_type = (buffer[pos] << 8) | buffer[pos + 1];
+        int ext_len = (buffer[pos + 2] << 8) | buffer[pos + 3];
+        pos += 4;
+        if (ext_type == 0) {
+            if (pos + 5 > bytes) return;
+            int name_len = (buffer[pos + 3] << 8) | buffer[pos + 4];
+            int name_start = pos + 5;
+
+            if (name_start + name_len > bytes) return;
+
+            printf("[%s] TLS SNI: ", timestr);
+            for (int i = 0; i < name_len; i++){
+                printf("%c", buffer[name_start + i]);
+            }
+            printf("\n");
+            return;
+        }
+        pos += ext_len;
+    }
+}
 int main (int argc, char *argv[]) {
 	int filter = 0;
 // Parse command arguments before opening the raw socket	
@@ -143,7 +185,19 @@ int main (int argc, char *argv[]) {
                         }
                         printf("\n");
                     }
-            }             
+            } 
+// TLS Parser
+            if(ntohs(tcp->source) == 443 || ntohs(tcp->dest) == 443){
+                int tcp_header_len = tcp->doff * 4;
+                int tls_offset = 14 + ip_header_len + tcp_header_len;
+                int tls_len = bytes - tls_offset;
+
+                if (tls_len > 5 && buffer[tls_offset] == 22) {
+                    if (tls_len > 5 && buffer[tls_offset] == 22){
+                        parse_tls_sni(buffer, bytes, tls_offset, timestr);
+                    }
+                }
+            }
         }
         else if (ip->protocol == 17){
             if (bytes < 14 + ip_header_len + (int)sizeof(struct udphdr)) continue;
